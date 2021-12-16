33d745eed481eac3fdff6ec0ed87e331ae6fa1ca
Leonid <leonid.cheshkov@mail.ru>
2021-12-16 21:32:49 +0300

    Igra

---
+++ public_html/wp-content/plugins/menu-icons/vendor/composer/installed.php
@@ -0,0 +1,70 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v0.12.7',
+    'version' => '0.12.7.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7aba45243d9fab287bf5479eff17fc9056736d9b',
+    'name' => 'codeinwp/wp-menu-icons',
+  ),
+  'versions' => 
+  array (
+    'codeinwp/gutenberg-menu-icons' => 
+    array (
+      'pretty_version' => '1.0.4',
+      'version' => '1.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '121ef82c57a556301265cbd1032d28619235e488',
+    ),
+    'codeinwp/icon-picker' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '0c60ce3a41653e41a20e710c4d5a6a2104c85020',
+    ),
+    'codeinwp/menu-item-custom-fields' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'f491fcfa873e92006e3d92a4ede8775e9cf53bae',
+    ),
+    'codeinwp/themeisle-sdk' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '24668ab249d8fb6596cc908c323205648aad0ed8',
+    ),
+    'codeinwp/wp-menu-icons' => 
+    array (
+      'pretty_version' => 'v0.12.7',
+      'version' => '0.12.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7aba45243d9fab287bf5479eff17fc9056736d9b',
+    ),
+    'composer/installers' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
